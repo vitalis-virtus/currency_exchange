c45d43ed1617119a08c078cf8d8b755ac6003989
vitalis-virtus <68068673+vitalis-virtus@users.noreply.github.com>
2022-07-16 14:07:27 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import * as currencyApi from "./services/services";
 
 import React, { useState, useEffect } from "react";
 
+import { GlobalSvgSelector } from "./assets/icons/global/GlobalSvgSelector";
+
 function App() {
   const [inputCurrencyCode, setInputCurrencyCode] = useState("USD");
   const [outputCurrencyCode, setOutputCurrencyCode] = useState("EUR");
@@ -10,7 +12,9 @@ function App() {
   const [inputCurrencyValue, setInputCurrencyValue] = useState("");
   const [outputCurrencyValue, setOutputCurrencyValue] = useState(0);
 
+  // additional info
   const [actualDate, setActualDate] = useState("");
+  const [rate, setRate] = useState("");
 
   const handleInputCodeChange = (event) => {
     setInputCurrencyCode(event.target.value);
@@ -40,13 +44,14 @@ function App() {
         const { data } = response;
         const exchangeResult = data.result;
         const exchangeActualDate = data.date;
-
-        console.log(response);
+        const rate = data.info.rate;
         if (exchangeResult) {
           setOutputCurrencyValue(exchangeResult.toFixed(2));
+          setRate(rate.toFixed(4));
           //we gonna change date format from 'yyyy-mm-dd' to 'dd.mm.yyyy'
         } else {
           setOutputCurrencyValue(0);
+          setRate(0);
         }
         setActualDate(exchangeActualDate.split("-").reverse().join("."));
       });
@@ -58,82 +63,101 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Калькулятор обміну(конвертації валют)</h1>
-      <h2>
-        Тут ви можете перегулянути актуальний курс для обміну однієї іноземної
-        валюти на іншу
-      </h2>
+      <div className="headers__wrapper">
+        <h1>Калькулятор обміну(конвертації валют)</h1>
+        <h2>
+          Тут ви можете перегулянути актуальний курс для обміну однієї іноземної
+          валюти на іншу
+        </h2>
+      </div>
 
       {/* input field */}
-      <div className="fieldsContainer">
-        <div className="inputCurrency">
-          <h3 className="inputCurrency__header">Ви віддаєте</h3>
-          <form onSubmit={preventEvent} className="inputCurrency__form">
-            <label htmlFor="inputValue">
-              <input
-                onChange={handleInputValueChange}
-                type="number"
-                id="inputValue"
-                name="inputValue"
-                placeholder="0"
-                value={inputCurrencyValue}
-                className="inputCurrency__input"
-              />
-            </label>
-            <label htmlFor="inputCurrencyCode">
-              <select
-                id="inputCurrencyCode"
-                value={inputCurrencyCode}
-                onChange={handleInputCodeChange}
-                className="inputCurrency__select"
+      <div className="content__wrapper">
+        <div className="fieldsContainer">
+          <div className="input-currency">
+            <h3 className="input-currency__header">Ви віддаєте</h3>
+            <form onSubmit={preventEvent} className="input-currency__form">
+              <label
+                htmlFor="inputValue"
+                className="input-currency__input__label"
               >
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-                <option value="NOK">NOK</option>
-                <option value="SEK">SEK</option>
-                <option value="DKK">DKK</option>
-              </select>
-            </label>
-          </form>
-        </div>
-        <button type="button" onClick={handleReverseChange}>
-          change
-        </button>
-
-        {/* output field */}
-
-        <div className="field">
-          <h3>Ви отримуюте</h3>
-          <div className="outputCurrencyContainer">
-            {inputCurrencyValue ? (
-              <p className="outputValue">{outputCurrencyValue}</p>
-            ) : (
-              <p className="outputValue">0</p>
-            )}
-            <form>
-              <select
-                value={outputCurrencyCode}
-                onChange={handleOutputCodeChange}
-                // defaultValue="EUR"
-              >
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-                <option value="NOK">NOK</option>
-                <option value="SEK">SEK</option>
-                <option value="DKK">DKK</option>
-              </select>
+                <input
+                  onChange={handleInputValueChange}
+                  type="number"
+                  id="inputValue"
+                  name="inputValue"
+                  placeholder="0"
+                  value={inputCurrencyValue}
+                  className="input-currency__input"
+                />
+              </label>
+              <label htmlFor="inputCurrencyCode">
+                <select
+                  id="inputCurrencyCode"
+                  value={inputCurrencyCode}
+                  onChange={handleInputCodeChange}
+                  className="input-currency__select"
+                >
+                  <option value="USD">USD</option>
+                  <option value="EUR">EUR</option>
+                  <option value="NOK">NOK</option>
+                  <option value="SEK">SEK</option>
+                  <option value="DKK">DKK</option>
+                  <option value="UAH">UAH</option>
+                  <option value="PLN">PLN</option>
+                </select>
+              </label>
             </form>
           </div>
+          <button
+            type="button"
+            onClick={handleReverseChange}
+            className="change-button"
+          >
+            <GlobalSvgSelector id="arrows" />
+          </button>
+
+          {/* output field */}
+
+          <div className="output-currency">
+            <h3 className="output-currency__header">Ви отримуєте</h3>
+            <div className="output-currency__output">
+              {inputCurrencyValue ? (
+                <p className="output-currency__value">{outputCurrencyValue}</p>
+              ) : (
+                <p className="output-currency__value">0</p>
+              )}
+              <form className="output-currency__form">
+                <label htmlFor="outputCurrencyCode">
+                  <select
+                    value={outputCurrencyCode}
+                    className="output-currency__select"
+                    id="outputCurrencyCode"
+                    onChange={handleOutputCodeChange}
+                  >
+                    <option value="USD">USD</option>
+                    <option value="EUR">EUR</option>
+                    <option value="NOK">NOK</option>
+                    <option value="SEK">SEK</option>
+                    <option value="DKK">DKK</option>
+                    <option value="UAH">UAH</option>
+                    <option value="PLN">PLN</option>
+                  </select>
+                </label>
+              </form>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="">
-        <p>Курс обміну актуальний на {actualDate} </p>
-        {inputCurrencyValue && (
-          <p>
-            1 {inputCurrencyCode}={outputCurrencyValue / inputCurrencyValue}
-            {outputCurrencyCode}
+        <div className="additional-info">
+          <p className="additional-info__date">
+            Курс обміну актуальний на {actualDate}{" "}
           </p>
-        )}
+          {inputCurrencyValue && (
+            <p className="additional-info__exchange-value">
+              1 {inputCurrencyCode} = {rate} {outputCurrencyCode}
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
