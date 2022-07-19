f94b0a6eab4049a393fc84dc5d6275bc3d3bacaf
vitalis-virtus <68068673+vitalis-virtus@users.noreply.github.com>
2022-07-19 15:11:46 +0300

    fixed swapButton

--- src/components/SwapButton/SwapButton.js
+++ src/components/SwapButton/SwapButton.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SwapButton.css";
 import { GlobalSvgSelector } from "../../assets/icons/global/GlobalSvgSelector";
 
 export default function SwapButton({ handleSwap }) {
+  const [swapped, setSwapped] = useState(false);
+
+  const handleSwapChange = () => {
+    console.log(swapped);
+    handleSwap();
+    setSwapped((prevState) => !prevState);
+  };
+
   return (
-    <button type="button" onClick={handleSwap} className="swap-button">
+    <button
+      type="button"
+      data-swapped={swapped}
+      onClick={handleSwapChange}
+      className="swap-button"
+    >
       <GlobalSvgSelector id="arrows" />
     </button>
   );
